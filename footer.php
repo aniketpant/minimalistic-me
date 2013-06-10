64c43678f1c1952cac4d83550647266c46b07aca
Aniket Pant <me@aniketpant.com>
2013-06-10 10:58:02 +0530

    Remove unnecessary code
    Signed-off-by: Aniket Pant <me@aniketpant.com>

--- footer.php
+++ footer.php
@@ -41,15 +41,8 @@
 
 </div> <!-- end #container -->
 
-<!-- scripts are now optimized via Modernizr.load -->
-<script src="<?php echo get_template_directory_uri(); ?>/library/js/scripts.js"></script>
-
-<!--[if lt IE 7 ]>
-	<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
-	<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
-  <![endif]-->
-
-  <?php wp_footer(); // js scripts are inserted using this function ?>
+<!-- all js scripts are loaded in library/bones.php -->
+<?php wp_footer(); ?>
 
 </body>
 
